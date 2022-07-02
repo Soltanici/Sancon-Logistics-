2b0e589ed3a63fe28a07a05e958338eef2781a56
Soltanici <100209269+Soltanici@users.noreply.github.com>
2022-07-02 15:10:56 +0100

    Working 1day!

---
+++ app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+import ploty.express as px
+from PIL import image
+
+st.set_page_config(page_title="Survey Rezults")
+st.header("Survey Result 2021")
+st.subheader("Was the utorial helpful?")
+
